b550f398a491c32691bcc2c87f0d6e38c8fb137c
JP-1997 <jyotiprakash.jpl.99@gmail.com>
2020-12-20 20:12:51 +0530

    deleting an existing class

---
+++ Migrations/202012201434543_DeleteCategoriesTable.cs
@@ -0,0 +1,42 @@
+namespace CodeFirstExistingDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeleteCategoriesTable : DbMigration
+    {
+        public override void Up()
+        {
+            /*Suppose we want to preserve the data (historical purposes maybe!) before deleting table*/
+            CreateTable(
+                "dbo._Categories",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(),
+                })
+                .PrimaryKey(t => t.Id);
+            Sql("INSERT INTO _Categories (Name) SELECT Name FROM Categories");
+            /*----------------------------------------------------------------------------------------*/
+
+            DropTable("dbo.Categories");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+
+            Sql("INSERT INTO Categories (Name) SELECT Name FROM _Categories");
+            DropTable("dbo._Categories");
+
+
+        }
+    }
+}
